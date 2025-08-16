5dca45d414b4629742cd43d4dda3328a72c9dade
melonlorrd <mprasadme@gmail.com>
2025-08-16 20:02:12 +0530

    update

--- sdl2Intro/sdl2.go
+++ sdl2Intro/sdl2.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+//	"math/rand"
 	"unsafe"
 
 	"github.com/veandco/go-sdl2/sdl"
@@ -12,7 +12,8 @@ const winWidth = 800
 const winHeight = 600
 
 type color struct {
-	r, g, b, a byte
+//	r, g, b, a byte
+	r, g, b byte
 }
 
 func setPixel(x, y int, c color, pixels []byte) {
@@ -29,7 +30,7 @@ func setPixel(x, y int, c color, pixels []byte) {
 	pixels[index] = c.r
 	pixels[index+1] = c.g
 	pixels[index+2] = c.b
-	pixels[index+3] = c.a
+//	pixels[index+3] = c.a
 }
 
 func main() {
@@ -54,16 +55,16 @@ func main() {
 	}
 	defer tex.Destroy()
 
-	pixels := make([]byte, winWidth*winHeight*4) // buffer = width * height * 4 as each pixel has 4 values R G B A
+	pixels := make([]byte, winWidth*winHeight*3) // buffer = width * height * 4 as each pixel has 4 values R G B A
 
 	for y := range winHeight {
 		for x := range winWidth {
-			// setPixel(x, y, color{255, 0, 0, 255}, pixels)
-			setPixel(x, y, color{byte(rand.Intn((256))), byte(rand.Intn((256))), byte(rand.Intn((256))), byte(rand.Intn((256)))}, pixels)
+			setPixel(x, y, color{255, 0, 0}, pixels)
+			//setPixel(x, y, color{byte(rand.Intn((256))), byte(rand.Intn((256))), byte(rand.Intn((256))), byte(rand.Intn((256)))}, pixels)
 		}
 	}
 
-	tex.Update(nil, unsafe.Pointer(&pixels[0]), winWidth*4)
+	tex.Update(nil, unsafe.Pointer(&pixels[0]), winWidth*3)
 	renderer.Copy(tex, nil, nil)
 	renderer.Present()
 	sdl.Delay(10000)
